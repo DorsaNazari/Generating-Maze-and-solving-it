8bde5180ed50a579b4ab5092cc208cca82dbe462
DorsaNazari <dorsanazari90@yahoo.com>
2021-05-22 02:29:04 +0430

    Let's start!

---
+++ cpp/maze.cpp
@@ -0,0 +1,17 @@
+#include "maze.h"
+#include<iostream>
+#include<vector>
+// #include <list>
+// #include <initializer_list>
+// #include<algorithm> 
+// #include<stdexcept>
+// // #include<compare>
+// #include <iterator>
+
+using Matrix = std ::vector <std :: vector<int>>;
+
+Matrix maze::create_maze(int row , int col){
+    std::cout<<"hello\n";
+    
+
+}
