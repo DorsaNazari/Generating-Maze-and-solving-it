6c8963abebd4ba12c4fd51c28d12d227c5237b42
DorsaNazari <dorsanazari90@yahoo.com>
2021-05-26 01:47:09 +0430

    We can move to possible cells in the maze!

--- h/maze.h
+++ h/maze.h
@@ -11,14 +11,14 @@ using Matrix = std ::vector <std :: vector<int>>;
 
 class maze{
     public:
-       Matrix create_maze(int row , int col);
-       void show(Matrix& a);
-       Matrix make_a_way(Matrix& a);
-       
-
-
+        Matrix create_maze(int row , int col);
+        void show(Matrix& a);
+        Matrix make_a_way(Matrix& a);
+        Matrix move(Matrix& a,int b);
+        std::vector<int> possible_moves(Matrix& a);
+        int x{};
+        int y{};
 
 };
-
 
 #endif
