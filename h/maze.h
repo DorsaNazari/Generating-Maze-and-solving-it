e79bb9b729e977f924262a28dffa4f501c94e9b9
DorsaNazari <dorsanazari90@yahoo.com>
2021-05-30 17:32:24 +0430

    bfs algorithm created!

--- h/maze.h
+++ h/maze.h
@@ -6,16 +6,23 @@
 // #include<initializer_list>
 // #include<set>
 #include<vector>
+#include <deque>
+
 
 using Matrix = std ::vector <std :: vector<int>>;
 
 class maze{
     public:
-        Matrix create_maze(int row , int col);
-        void show(Matrix& a);
-        Matrix make_a_way(Matrix& a);
-        Matrix move(Matrix& a,int b);
+        // maze(maze&);
+        maze* create_maze(int row , int col);
+        void show();
+        maze* make_a_way();
+        maze* move(int b);
+        bool end (std::deque<maze*> d);
         std::vector<int> possible_moves(Matrix& a);
+        std::deque<maze> bfs (std::deque<maze> &mazes);
+        Matrix a{};
+        std::deque<maze> create_deque (){return std::deque<maze> {*this};} ;     // std::deque<maze> mazes {*this};
         int x{};
         int y{};
 
