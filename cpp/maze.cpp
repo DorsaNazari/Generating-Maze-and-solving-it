5d1573c4b0670a2ea729fe447f87ca3bcc8996fd
DorsaNazari <dorsanazari90@yahoo.com>
2021-06-05 21:49:19 +0430

    bidirectional is working!

--- cpp/maze.cpp
+++ cpp/maze.cpp
@@ -4,10 +4,7 @@
 #include <time.h>
 #include<iomanip>
 #include <deque>
-// #include <list>
-// #include <initializer_list>
-// #include<algorithm> 
-// #include<stdexcept>
+
 
 using Matrix = std ::vector <std :: vector<int>>;
 
@@ -120,16 +117,16 @@ void maze::show2()
     }
 }
 
-void maze::show(){
-    for(size_t i{0} ;i<a.size() ;i++)
+void maze::show(Matrix& u){
+    for(size_t i{0} ;i<u.size() ;i++)
     {
-        for(size_t j{0};j<a[i].size();j++){
+        for(size_t j{0};j<u[i].size();j++){
             // std::cout<< std::setiosflags(std::ios::left)<< std::setw(2)<< a[i][j] ;
-            if(a[i][j] == 1)
+            if(u[i][j] == 1)
                 std::cout<< "#" ;
-            if(a[i][j] == 0)   
+            if(u[i][j] == 0)   
                 std::cout<< "-" ;
-            if(a[i][j] == 5){
+            if(u[i][j] == 5){
                 std::cout<< "\u001b[31m$" ;
                 std::cout<<"\u001b[0m";
             }
@@ -184,6 +181,11 @@ maze* maze::make_a_way(){
             a[row][col] = 0; 
         }       
     }
+    c = a;
+    c[0][0] = 0;
+    c[row][col] = 5;
+    x_c = row;
+    y_c = col;
     return this;
 }
 
@@ -251,6 +253,73 @@ maze* maze::move(int b){
     return this;
 }
 
+maze* maze::move_c(int b){
+    //0->up  1->right  2->down  3->left   7->start
+    //start
+    // a[x][y] = 0;
+    if(b == 7){
+        x_c = 0;
+        y_c = 0;
+        c[x_c][y_c] = 5;
+    }
+    //up
+    if(b == 0){
+        if(x_c -1 >= 0)
+        // if(x_c -1 < static_cast<int>(c.size()))
+        {
+            if( c[x_c -1][y_c] == 0 ){
+                c[x_c -1][y_c] = 5;
+                x_c -= 1;
+            }
+            else
+                std::cout<<"There's not a way!\n";
+        }
+        else
+            std::cout<<"out of band!\n";
+    }
+    //right
+    if(b == 1){
+        if(y_c +1 < static_cast<int>(c[0].size())){
+            if( c[x_c][y_c+1] == 0 ){
+                c[x_c][y_c+1] = 5;
+                y_c +=1;
+            }
+            else
+                std::cout<<"There's not a way!\n";
+        }
+        else
+            std::cout<<"out of band!\n";
+    }
+    //down
+    if(b == 2){
+        if(x_c + 1 < static_cast<int>(c.size())){
+            if( c[x_c+1][y_c] == 0 ){
+                c[x_c+1][y_c] = 5;
+                x_c += 1;
+            }
+            else
+                std::cout<<"There's not a way!\n";
+        }
+        else
+            std::cout<<"out of band!\n";
+    }
+    //left
+    if(b == 3){
+        if(y_c-1 >= 0){
+            if( c[x_c][y_c-1] == 0 ){
+                c[x_c][y_c-1] = 5;
+                y_c -= 1;
+            }
+            else    
+                std::cout<<"There's not a way!\n";
+        }
+        else
+            std::cout<<"out of band!\n";
+    }   
+    // std::cout<<"move ccc\n";
+    return this;
+}
+
 std::vector<int> maze::possible_moves(){
     std::vector<int> dir;
     if(a[x][y] != 1 ){
@@ -280,7 +349,36 @@ std::vector<int> maze::possible_moves(){
     return dir;
 }
 
-std::deque<maze> maze::bfs (std::deque<maze> mazes){
+std::vector<int> maze::possible_moves_c(){
+    std::vector<int> dir;
+    if(c[x_c][y_c] != 1 ){
+        if(x_c -1 >= 0){
+            if( c[x_c -1][y_c] == 0 )
+                dir.push_back(0);  
+        }
+        if(y_c +1 < static_cast<int>(c[0].size())){
+            if( c[x_c][y_c+1] == 0 )
+                dir.push_back(1);
+        }
+        if(x_c+1 < static_cast<int>(c.size())){
+            if( c[x_c+1][y_c] == 0 )
+                dir.push_back(2);
+        }
+        if(y_c-1 >= 0){
+            if( c[x_c][y_c-1] == 0 )
+                dir.push_back(3);
+        }
+    }
+    else
+        std::cout<<"this cell is full!\n";
+    // for(auto i:dir){
+    //     std::cout<<"dir : "<<i<<" ";
+    // }
+    // std::cout<<std::endl;
+    return dir;
+}
+
+std::deque<maze> maze::bfs(std::deque<maze> mazes){
     if(mazes.size()==0){
         std::cout<<"no way\n";
         return mazes;
@@ -330,7 +428,7 @@ bool maze::visited (std::vector<int> k){
     return is_visited;
 }
 
-std::deque<maze> maze::dfs (std::deque<maze>&mazes){
+std::deque<maze> maze::dfs (std::deque<maze>&mazes,int d){
     maze m = mazes[0];
     static int depth{};
     if(m.a[a.size()-1][a[0].size()-1] == 5){
@@ -338,7 +436,7 @@ std::deque<maze> maze::dfs (std::deque<maze>&mazes){
         m.show2();
         return mazes;
     }
-    if(depth > 110){
+    if(depth > d){
         std::cout<<"answer not found!\n";
         return mazes;
     }
@@ -349,7 +447,7 @@ std::deque<maze> maze::dfs (std::deque<maze>&mazes){
             mazes.push_back(*n.move(j));
             depth += 1;
             mazes.pop_front();
-            std::deque<maze> k = n.dfs(mazes);
+            std::deque<maze> k = n.dfs(mazes,d);
             depth -= 1;
             if(k[0].a[a.size()-1][a[0].size()-1] == 5){
                 return k;
@@ -357,4 +455,117 @@ std::deque<maze> maze::dfs (std::deque<maze>&mazes){
         }
     }
     return mazes;
+}
+
+std::vector<std::deque<maze>> maze::bidirectional_ (std::vector<std::deque<maze>> z){
+    if(z[0].size()==0 || z[1].size()==0 ){
+        std::cout<<"no way\n";
+        show(a);
+        show(c);
+        return z;
+    }
+    maze m1 = z[0][0];
+    maze m2 = z[1][0];
+    //if its end return the deque
+    if( comparison(z[0],z[1])[0] != -1 ){
+        std::cout<<"solution is found\n";
+        m1.show(a);
+        std::cout<<"*********\n";
+        return z;
+    }
+    else{
+    //else delete first maze and move to possible cells then run bfs on elements of deque again
+        std::vector<int> v1 = m1.possible_moves();
+        std::vector<int> v2 = m2.possible_moves_c();
+        // std::cout<<v[0]<<"\n";
+        for(auto j1:v1){
+            std::vector<int> l1{};
+            maze n1 = m1;
+            n1.move(j1);
+            l1.push_back(n1.x);
+            l1.push_back(n1.y);
+            // if(!(visited(l1)))
+                z[0].push_back(n1);
+        }
+        for(auto j2:v2){
+            std::vector<int> l2{};
+            maze n2 = m2;
+            n2.move_c(j2);
+            l2.push_back(n2.x_c);
+            l2.push_back(n2.y_c);
+            // if(!(visited(l2)))
+                z[1].push_back(n2);
+        }
+        z[0].pop_front();
+        z[1].pop_front();
+        m1.bidirectional_(z);
+    }
+    return z;
+}
+
+std::vector<std::deque<maze>> maze::bidirectional (std::vector<std::deque<maze>> z){
+    if(z[0].size()==0 || z[1].size()==0 ){
+        std::cout<<"no way\n";
+        show(a);
+        show(c);
+        return z;
+    }
+    maze m1 = z[0][0];
+    maze m2 = z[1][0];
+    //if its end return the deque
+    if( comparison(z[0],z[1])[0] != -1 ){
+        std::cout<<"solution is found\n";
+        // m1.show(a);
+        std::cout<<"*********\n";
+        m2.show(c);
+        return z;
+    }
+    else{
+    //else delete first maze and move to possible cells then run bfs on elements of deque again
+        std::vector<int> v1 = m1.possible_moves();
+        std::vector<int> v2 = m2.possible_moves_c();
+        // std::cout<<v[0]<<"\n";
+        for(auto j1:v1){
+            std::vector<int> l1{};
+            maze n1 = m1;
+            n1.move(j1);
+            l1.push_back(n1.x);
+            l1.push_back(n1.y);
+            // if(!(visited(l1)))
+                z[0].push_back(n1);
+        }
+        for(auto j2:v2){
+            std::vector<int> l2{};
+            maze n2 = m2;
+            n2.move_c(j2);
+            l2.push_back(n2.x_c);
+            l2.push_back(n2.y_c);
+            // if(!(visited(l2)))
+                z[1].push_back(n2);
+        }
+        z[0].pop_front();
+        z[1].pop_front();
+
+        m2.bidirectional(z);
+    }
+    return z;
+}
+
+std::vector<int> maze::comparison(std::deque<maze> mazes1,std::deque<maze> mazes2){
+    std::vector<int> v;
+    for(size_t i{};i<mazes1.size();i++){
+        for(size_t j{};j<mazes2.size();j++)
+        {
+            if(mazes1[i].x == mazes2[j].x_c && mazes1[i].y == mazes2[j].y_c){
+                v.push_back(i);
+                v.push_back(j);
+                // mazes1[i].show(a);
+                // mazes2[j].show(c);
+                // std::cout<<"compariccccccccc"<<mazes2[j].x_c<<mazes2[j].y_c<<"aaaaaaa"<<mazes1[i].x<<mazes1[i].y<<"\n";
+                return v; 
+            }
+        }
+    }
+    v.push_back(-1);
+    return v;
 }
