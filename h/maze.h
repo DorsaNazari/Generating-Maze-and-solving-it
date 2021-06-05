185e0c5f130bea3fd92a7d26474f926cad5fa857
DorsaNazari <dorsanazari90@yahoo.com>
2021-06-05 21:49:37 +0430

    bidirectional is working!

--- h/maze.h
+++ h/maze.h
@@ -1,10 +1,6 @@
 #ifndef MAZE_H
 #define MAZE_H
 #include<iostream>
-// #include <list>
-// // #include<compare>
-// #include<initializer_list>
-// #include<set>
 #include<vector>
 #include <deque>
 
@@ -13,23 +9,28 @@ using Matrix = std ::vector <std :: vector<int>>;
 
 class maze{
     public:
-        // maze(maze&);
         maze* create_maze(int row , int col);
-        void show();
+        void show(Matrix& a);
         void show2();
         maze* make_a_way();
         maze* move(int b);
-        bool end (std::deque<maze*> d);
+        maze* move_c(int b);
         std::vector<int> possible_moves();
+        std::vector<int> possible_moves_c();
         std::deque<maze> bfs (std::deque<maze> mazes);
         bool visited (std::vector<int> k);
-        std::deque<maze> dfs (std::deque<maze>&mazes);
+        std::deque<maze> dfs (std::deque<maze>&mazes,int d);
+        std::vector<std::deque<maze>> bidirectional (std::vector<std::deque<maze>> z);
+        std::vector<std::deque<maze>> bidirectional_ (std::vector<std::deque<maze>> z);
+        std::vector<int> comparison(std::deque<maze> mazes1,std::deque<maze> mazes2);
         Matrix a{};
-
-        // static Matrix visitedd;
+        Matrix c{};
         std::deque<maze> create_deque (){return std::deque<maze> {*this};} ;
         int x{};
         int y{};
+        int x_c{};
+        int y_c{};
+
 
 };
 
